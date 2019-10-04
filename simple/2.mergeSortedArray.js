405b57a1ba98a64e9fbb523a04d1970f9a3d4bc9
won1lgl <2192171643@qq.com>
2019-10-04 14:53:54 +0800

    改进了一下算法，不会生成新的数组

--- simple/2.mergeSortedArray.js
+++ simple/2.mergeSortedArray.js
@@ -3,7 +3,8 @@
 
 // 我觉得最简单的实现方法（使用js现成的工具）缺点：生成了新的数组，不符合题意
 const MergeSortedArray = (arr1, arr2) => {
-    return [...arr1, ...arr2].sort((a, b) => a - b)
+    arr1.push(...arr2)
+    return arr1.sort((a, b) => a - b)
 }
 
 const testArray1 = [1, 200, 2000, 8000]
@@ -12,7 +13,6 @@ const testArray2 = [-5, -3, 200, 1000, 1200, 10000]
 console.log(MergeSortedArray(testArray1, testArray2))
 
 // 我写的传统算法,相当于传统意义上的两个指针
-
 const MergeSortedArray2 = (arr1, arr2) => {
     let i = 0
     for (const j of arr2) {
